b59f8935d07ee83990b69a6fa52fc65857c83529
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-07 03:10:24 -0600

    updated root index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    // required for testing
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
 
     //require_once '../../config/DatabaseLocal.php';
     require_once '../../config/Database.php';
