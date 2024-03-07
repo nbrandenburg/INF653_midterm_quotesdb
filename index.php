49ba175d1a32f1a82cdabc5fd51134b8044cced1
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-07 03:04:05 -0600

    switch from local to online database

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     header('Content-Type: application/json');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 
-    require_once '../../config/DatabaseLocal.php';
-    //require_once '../../config/Database.php';
+    //require_once '../../config/DatabaseLocal.php';
+    require_once '../../config/Database.php';
     require_once '../../api/authors/';
     require_once '../../api/categories/';
     require_once '../../api/quotes/';
