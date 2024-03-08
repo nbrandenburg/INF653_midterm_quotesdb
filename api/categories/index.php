9b0d105af19ba50b79d4d0b5cb427c2721d5edfe
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-08 04:48:24 -0600

    updated headers

--- api/categories/index.php
+++ api/categories/index.php
@@ -1,6 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
     $method = $_SERVER['REQUEST_METHOD'];
 
     if ($method === 'OPTIONS') {
