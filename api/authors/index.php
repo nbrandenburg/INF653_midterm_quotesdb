8d7f5098ec92719114386f70ab18d503554bebf2
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-08 04:17:27 -0600

    updates to index files

--- api/authors/index.php
+++ api/authors/index.php
@@ -15,6 +15,7 @@
 
     if ($method === 'GET') {
         header('Access-Control-Allow-Methods: GET');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         if(!isset($_GET['id'])) {
             require_once 'read.php';
         } else {
@@ -24,15 +25,18 @@
 
     elseif ($method === 'POST') {
         header('Access-Control-Allow-Methods: POST');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         require_once 'create.php';
     }
 
     elseif ($method === 'PUT') {
         header('Access-Control-Allow-Methods: PUT');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         require_once 'update.php';
     }
 
     elseif ($method === 'DELETE') {
         header('Access-Control-Allow-Methods: DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         require_once 'delete.php';
     }
