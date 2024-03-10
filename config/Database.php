48c40c62302bf3ea5db403507e2d75b575c23314
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-09 22:29:44 -0600

    updated Database.php

--- config/Database.php
+++ config/Database.php
@@ -21,14 +21,13 @@
                 // connection already exists, return it
                 return $this->conn;
             } else {
-                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+                $dsn = "pgsql:host={$this->host};dbname={$this->db_name}";
 
                 try {
                     $this->conn = new PDO($dsn, $this->username, $this->password);
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     return $this->conn;
                 } catch(PDOException $e) {
-                    // echo for tutorial, log for production
                     echo 'Connection Error: ' . $e->getMessage();
                 }            
             }            
