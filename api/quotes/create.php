430f7e1b5393c8e319f99d94d2622ff44e0ca58b
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-03-13 12:05:37 -0500

    create and update methods

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -10,6 +10,21 @@
     $quote->author_id = htmlspecialchars(strip_tags($data['author_id']));
     $quote->category_id = htmlspecialchars(strip_tags($data['category_id']));
 
+    // Make sure author_id is valid
+    $author_arr -> $author->read_single($quote->author_id);
+    if($quote->author_id != $author_arr['id']) {
+        echo json_encode($author_arr);
+        exit();
+    }
+    
+    // Make sure category_id is valid
+    $category_arr -> $category->read_single($quote->category_id);
+    if($quote->category_id != $category_arr['id']) {
+        echo json_encode($category_arr);
+        exit();
+    }   
+    
+
     // Create quote
     try {
         if($quote->quote == NULL) {
